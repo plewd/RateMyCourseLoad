f7c35bc6b17aa3428ac2d3f7ebc46905a711ecce
nineluj <julian.hirn@protonmail.com>
2024-03-09 17:00:20 +0100

    added website hosting for api and main site (main site doesn't have https support yet)

---
+++ scripts/deploy_frontend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+cd ratemycourseload-frontend
+
+BUILD_DIR="dist/"
+
+echo "--- Building project"
+npm run build
+echo "--- Syncing files from build directory ($BUILD_DIR) to S3"
+aws s3 sync "$BUILD_DIR" "s3://ratemycourseload.com/"
+echo "--- Done"
